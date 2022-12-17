28477b74ea7befeea2f1798e567289d8e65b6bb9
[mercadohectordw] <[mercadohectordw@gmail.com]>
2022-12-16 21:41:31 -0300

    usuarios se registran y se buscan en bd

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -3,18 +3,55 @@ import { loginValidation, registerValidation } from "../middleware/validation"
 
 export const loginUser = (req:any, res:any) => {
   let logData = req.body;
-
   let validation = loginValidation(logData);
 
-  res.status(200).send(validation);
+  if(validation.error){
+    return res.status(400).send(validation.error);
+  }
+
+  let query = `
+    SELECT * FROM users
+    WHERE email = "${logData.email}"
+    AND password = "${logData.password}"
+  `;
+
+  db.query(query)
+    .then(([row]) => {
+      let result = JSON.parse(JSON.stringify(row));
+
+      if(result.length <= 0){
+        res.status(400).send("Usuario no encontrado");
+      } else {
+        res.status(200).send("Usuario encontrado");
+      }
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 };
 
 export const registerUser = (req:any, res:any) => {
   let userData = req.body;
+  let validation = registerValidation(userData);
+  
+  if(validation.error){
+    return res.status(400).send(validation.error);
+  }
+  
+  let query = `
+    INSERT INTO users (first_name, last_name, email, password, created_at, image_url) VALUES
+    ("${userData.first_name}", "${userData.last_name}", "${userData.email}", "${userData.password}", CURRENT_TIMESTAMP, "img")
+  `;
 
-  let validation = registerValidation(userData)
-
-  res.status(200).send(validation);
+  db.query(query)
+    .then(([row]) => {
+      let result = JSON.parse(JSON.stringify(row));
+      
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 };
 
 export const getAllUsers = (req:any, res:any) => {
